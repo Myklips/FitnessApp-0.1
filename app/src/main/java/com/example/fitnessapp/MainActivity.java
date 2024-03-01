87cacb61bdc5262f76a19e198c70ebb4a7c88a4a
Tristan Jones <tristan@jonesee.co.uk>
2024-03-01 18:11:43 +0000

    Created Calorie tracker

--- app/src/main/java/com/example/fitnessapp/MainActivity.java
+++ app/src/main/java/com/example/fitnessapp/MainActivity.java
@@ -4,11 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void CalorieClicked(View view){
+
+    }
+
+    public void WorkoutsClicked(View view){
+
     }
 }
