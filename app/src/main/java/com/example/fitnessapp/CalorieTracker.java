d0f0731a0baa406e080ebca1f832c328ed1b404a
Tristan Jones <tristan@jonesee.co.uk>
2024-03-06 14:57:52 +0000

    Created Calorie Counter layout and added main menu functionality

--- app/src/main/java/com/example/fitnessapp/CalorieTracker.java
+++ app/src/main/java/com/example/fitnessapp/CalorieTracker.java
@@ -3,6 +3,8 @@ package com.example.fitnessapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class CalorieTracker extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class CalorieTracker extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calorie_tracker);
+    }
+    public int CaloriesTracked;
+    public void MainMenuB(View v){
+        Intent MainMenuC = new Intent(this, MainActivity.class);
+        startActivity(MainMenuC);
+
     }
 }
