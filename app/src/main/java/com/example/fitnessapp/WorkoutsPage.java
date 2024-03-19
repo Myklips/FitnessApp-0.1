fff2deadc5c9e5e1b4b94c18abdd7ea8dbd2e38f
Tristan Jones <tristan@jonesee.co.uk>
2024-03-19 13:46:18 +0000

    Copied Main Menu button from Calorie tracker to Workouts Page

--- app/src/main/java/com/example/fitnessapp/WorkoutsPage.java
+++ app/src/main/java/com/example/fitnessapp/WorkoutsPage.java
@@ -2,7 +2,9 @@ package com.example.fitnessapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class WorkoutsPage extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class WorkoutsPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workouts_page);
+    }
+
+    public void MainMenuB(View v){
+
+        Intent MainMenuC = new Intent(this, MainActivity.class);
+        startActivity(MainMenuC);
+        //This is for the main menu button
     }
 }
